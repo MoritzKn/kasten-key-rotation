84baa930c1d5d6f1a2f1c327f0658e89a8556f04
Moritz Kneilmann <moritz@kneilmann.tech>
2022-03-05 18:37:38 +0100

    Add Vault script

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# kasten-key-rotation
-Scripts for automating key rotation in Kasten K10
+# Kasten K10 Key Rotation
+
+Scripts for automating key rotation in Kasten K10.
+
+## Vault
+
+See the [Kasten docs](https://docs.kasten.io/latest/install/configure.html#hashicorp-vault-transit-secrets-engine) on how to setup encryption using Vault in Kasten and the [Vault docs on key rotation in the Transit Secret Engine](https://learn.hashicorp.com/tutorials/vault/eaas-transit#rotate-the-encryption-key).
+
+If you want to rotate the Vault key used in Kasten, you have to follow a three step process:
+
+1. Create a new version of the key in Vault (i.e. trigger the `/rotate` API)
+2. Create a new Passkey to force Kasten to re-encrypt the master key
+3. Bump the min decryption version in Vault.
+
+The old key can then be deleted.
+
+If you have a Passkey yaml file, the following script will automatically do the above:
+
+However:
+
+1. Make sure kubectl is set to the right context
+2. Make sure `vault` is connected to your instance
+
+```sh
+./rotate-vault-key.sh passkey-vault-example.yaml
+```
+
+You can also use `VAULT_CMD` to override the Vault command.
+For example if you are running vault in Kubernetes:
+
+```sh
+export VAULT_CMD="kubectl --context vault-cluster-context -n vault exec -i vault-0 -- vault"
+./rotate-vault-key.sh passkey-vault-example.yaml
+```
+
+## KMS
+
+Coming soon
---
+++ rotate-vault-key.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+escape_sed() {
+  echo $1 | sed -e 's/[\/&]/\\&/g'
+}
+
+log_file="$(mktemp)"
+on_error() {
+    echo " > Failed!"
+    cat $log_file
+    exit 1
+}
+
+log_cmd() {
+  echo " >" "$@" >&2
+  "$@" > $log_file 2>&1 || on_error
+}
+
+log_cmd_pipe() {
+  echo " >" "$@" >&2
+  "$@"
+}
+
+passkey_file="$1"
+
+if [ ! -f "$passkey_file" ]; then
+    echo "No such file: '$passkey_file'"
+    echo "Ussage: ./rotate-vault-key.sh passkey.yaml"
+    exit 1
+fi
+
+vault_cmd=$VAULT_CMD
+if [ -z "$vault_cmd" ]; then
+    vault_cmd="vault"
+fi
+
+echo "* Checking Vault connection..."
+if $vault_cmd status > /dev/null; then
+    # echo "Connected to Vault!"
+    # Do not do anything
+    true
+else
+    echo " - Can not connect to Vault!"
+    echo " - Failed to run \$VAULT_CMD:"
+    echo " - $vault_cmd status"
+    exit 1
+fi
+
+passkey_json_file_temp="$(mktemp)"
+kubectl create -f $passkey_file --dry-run=client -o json > $passkey_json_file_temp
+
+vault_transit_path=$(jq .spec.vaulttransitpath -r < $passkey_json_file_temp)
+vault_key_name=$(jq .spec.vaulttransitkeyname -r < $passkey_json_file_temp)
+passkey_name_old=$(jq .metadata.name -r < $passkey_json_file_temp)
+rm $passkey_json_file_temp
+
+if [ "$vault_transit_path" = "null" ]; then
+    echo "  - Error: No '.spec.vaulttransitpath' found in $passkey_file"
+    exit 1
+fi
+if [ "$vault_key_name" = "null" ]; then
+    echo "  - Error: No '.spec.vaulttransitkeyname' found in $passkey_file"
+    exit 1
+fi
+
+vault_min_decryption_version=$($vault_cmd read $vault_transit_path/keys/$vault_key_name -format=json | jq .data.min_decryption_version)
+echo "* Min decryption version was: $vault_min_decryption_version"
+
+echo "* Rotating key..."
+log_cmd $vault_cmd write -f  $vault_transit_path/keys/$vault_key_name/rotate
+vault_key_latest_version=$($vault_cmd read $vault_transit_path/keys/$vault_key_name -format=json | jq .data.latest_version)
+echo "* New version is: $vault_key_latest_version"
+
+passkey_name_new=
+if echo $passkey_name_old | grep -E -- '_v\d*' > /dev/null; then
+    last_version=$(echo $passkey_name_old | grep -o -E -- '_v\d*')
+    new_version="_v$vault_key_latest_version"
+    passkey_name_new=$(echo $passkey_name_old | sed "s/$(escape_sed $last_version)/$(escape_sed $new_version)"/)
+else
+    passkey_name_new="${passkey_name_old}_v$vault_key_latest_version"
+fi
+
+echo "* Renaming Passkey: $passkey_name_old -> $passkey_name_new"
+new_passkey_file="$(mktemp)"
+log_cmd_pipe sed "s/$(escape_sed $passkey_name_old)/$(escape_sed $passkey_name_new)/" $passkey_file > $new_passkey_file
+log_cmd mv $new_passkey_file $passkey_file
+
+echo "* Updating Passkey..."
+log_cmd kubectl create -f $passkey_file
+
+echo "* Checking key..."
+status_file_temp=$(mktemp)
+passkey_is_inuse=false
+for i in 1 2 3 4 5; do
+    log_cmd_pipe kubectl get passkeys.vault.kio.kasten.io $passkey_name_new -o json > $status_file_temp
+    passkey_is_inuse=$(jq .status.inuse < $status_file_temp)
+    if [ "$passkey_is_inuse" = "true" ]; then
+        echo " - The used Passkey now is '$passkey_name_new'"
+        break
+    fi
+    echo " - Not yet inuse, trying again in 1 second..."
+    sleep 1
+done
+rm $status_file_temp
+
+if [ "$passkey_is_inuse" != "true" ]; then
+    echo " - Error: Passkey '$passkey_name_new' still not active after 5 tries"
+    exit 1
+fi
+
+echo "* Setting min decryption version to: $vault_key_latest_version"
+log_cmd $vault_cmd write $vault_transit_path/keys/$vault_key_name/config min_decryption_version=$vault_key_latest_version
+
+echo "* Deleting the old Passkey"
+log_cmd kubectl delete passkeys.vault.kio.kasten.io $passkey_name_old
+
+rm $log_file
